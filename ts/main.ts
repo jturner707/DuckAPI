10178fb623ac2fb17401823c8cbf945d287d483d
Jeff <jeffrey.turner@cptc.edu>
2024-11-21 11:14:45 -0800

    Initial getRandomDuck implemented CORS problem needs testing

--- ts/main.ts
+++ ts/main.ts
@@ -26,3 +26,27 @@ class RandomDuck {
 // example of how to tie (consume) the api to the class
 // let data: RandomDuck = new RandomDuck("This is a random duck", "https://random-d");
 
+
+// .then methods allow the program to wait for the info to be fetched.  It could take seconds.
+// this is considered asynchronous programming.
+function getDuck(): Promise<RandomDuck> {
+    return fetch('https://random-d.uk/api/v2/random', { mode: 'no-cors'})
+        .then(response => response.json())
+        .then((data: RandomDuck) => {
+            return new RandomDuck(data.message, data.url);
+        });
+}
+
+
+// ai can refactor this without lambda functions but it is longer
+// returns a promise that resolves to a RandomDuck object
+/**
+ * @returns a promise
+ */
+function main() {
+    getDuck().then((duck: RandomDuck) => {
+        console.log(duck);
+        console.log(duck.message);
+        console.log(duck.url);
+    });
+}
